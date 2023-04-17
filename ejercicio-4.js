32e1c156305e1b0c6495d063fe77b87422251e26
Jhonatan Quevedo <129713820+JhonatanQj@users.noreply.github.com>
2023-04-16 23:34:45 -0500

    Ejercicio 4

---
+++ ejercicio-4.js
@@ -0,0 +1,29 @@
+let cadenaTexto = "Hola mundo!";
+console.log(cadenaTexto)
+console.log(typeof (cadenaTexto))
+
+//let nombre = "Camilo"
+//cadenaTexto= "Hola!" + " Mi nombre es " + nombre
+//console.log(cadenaTexto)
+
+//let edad= 23
+//console.log(`${nombre} tiene ${edad} años`)
+//console.log(typeof(edad))
+
+let verdadero = true
+console.log("El verdadero se define con",verdadero)
+console.log(typeof(verdadero))
+
+let color;
+console.log(`Mi color favorito es ${color}`)
+console.log(typeof (color))
+
+let nombre = "Jhonatan";
+let apellido = "Quevedo";
+let edad = 27;
+let canción = "The monster";
+let respuesta = true
+
+console.log(`Hola! Mi nombre es ${nombre} ${apellido} y tengo ${edad} años, mi canción favorita es ${canción} y la respuesta es ${respuesta}`)
+
+
