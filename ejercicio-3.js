a20b2d15a1175b06832248ad74c69d1c71b0fe45
Jhonatan Quevedo <129713820+JhonatanQj@users.noreply.github.com>
2023-04-16 20:38:50 -0500

    Ejercicio 3

---
+++ ejercicio-3.js
@@ -0,0 +1,9 @@
+const PI = 3.1416
+console.log(`El valor de PI es: ${PI}`)
+
+const segundos_en_un_minuto = 60
+const minutos_en_una_hora = 60
+const horas_en_un_dia = 24
+
+console.log(`Un minuto tiene ${segundos_en_un_minuto} segundos, una hora tiene ${minutos_en_una_hora} minutos y un día tiene ${horas_en_un_dia} horas`)
+
