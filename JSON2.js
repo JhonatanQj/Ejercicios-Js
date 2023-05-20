54a1abf2ae5146e1e38a854a17e5d7c2b8a7ccd6
Jhonatan Quevedo <129713820+JhonatanQj@users.noreply.github.com>
2023-05-20 11:37:59 -0500

    Ejercicios DOM

--- JSON2.js
+++ JSON2.js
@@ -21,9 +21,9 @@ console.log("-----------Conversión de un Objeto a JSON----------------");
 
 //Objeto persona
 const persona = {
-  nombre: "Mariana",
-  edad: 36,
-  amigos: ["Juan", "Fernanda", "José"],
+        nombre: "Mariana",
+        edad: 36,
+        amigos: ["Juan", "Fernanda", "José"],
 };
 
 //JSON.stringify(obj). Convierte un objeto Javascript a JSON y la devuelve.
@@ -33,7 +33,7 @@ console.log(jsonPersona); //Impresion del json
 console.log(typeof persona);
 console.log(typeof jsonPersona);
 
-//Ejercicio 3 del Taller JSON2
+//Ejercicio 3 del Taller JSON1
 const informacion = `{
     "nombre": "Mike", 
     "apellido": "Munsvil", 
@@ -51,7 +51,7 @@ const informacion = `{
 
 console.log(informacion);
 
-//Ejercicio 4 del Taller JSON2
+//Ejercicio 4 del Taller JSON1
 
 let tienda = `{"Productos": [
         {
@@ -135,3 +135,105 @@ let tienda = `{"Productos": [
 }`
 
 console.log(tienda);
+
+
+//Ejercicio 4 (a) del Taller JSON2
+
+const Usuariop = [
+{
+        nombre: "Mariana",
+        apellido: "Díaz",
+        correo: "m.d@gmail.com",
+        telefono: 4210305,
+        nacionalidad: "Colombiana"
+},
+{
+        nombre: "José",
+        apellido: "González",
+        correo: "j.g@gmail.com",
+        telefono: 4210306,
+        nacionalidad: "Chileno"
+},
+{
+        nombre: "Dora",
+        apellido: "Jiménez",
+        correo: "d.j@gmail.com",
+        telefono: 4210306,
+        nacionalidad: "Peruana"
+},
+{
+        nombre: "Alba",
+        apellido: "Quevedo",
+        correo: "a.q@gmail.com",
+        telefono: 4210307,
+        nacionalidad: "Ecuatoriana"
+},
+{
+        nombre: "Simón",
+        apellido: "Pérez",
+        correo: "s.p@gmail.com",
+        telefono: 4210308,
+        nacionalidad: "Argentino"
+}];
+
+console.log(Usuariop);
+
+//Ahora se pasará a JSON
+const jsonUsuariop = JSON.stringify(Usuariop); //Conversión del objeto a json
+console.log(jsonUsuariop); //Impresion del json
+
+console.log(typeof Usuariop);
+console.log(typeof jsonUsuariop);
+
+
+
+//FUNCIONALIDAD PUSH
+let Frutas = []; // Se define una variable sin contenido por ejemplo...
+let fruta1 = "mango" // se define otra variable que más adelante se agregaría dentro de la variale "Frutas"
+
+Frutas.push(fruta1); // Se agrega la variable "fruta1" dentro de "Frutas"
+console.log(Frutas); // "fruta1" ya pertenece a "Frutas"
+
+
+
+//Ejercicio 4 (b) del Taller JSON2
+
+const jsonPeliculas = `[{
+        "Nombre": "Interstellar",
+        "Director": "Christopher Nolan",
+        "Musica": "Hans Zimmer",
+        "Anio": 2014
+},
+{
+        "Nombre": "El curioso caso de Benjamin Button",
+        "Director": "David Fincher",
+        "Musica": "Alexandre Desplat",
+        "Anio": 2008
+},
+{
+        "Nombre": "The Notebook",
+        "Director": "Nick Cassavetes",
+        "Musica": "Aaron Zigman",
+        "Anio": 2004
+},
+{
+        "Nombre": "Predestination",
+        "Director": ["Peter Spierig", "Michael Spierig"],
+        "Musica": "Peter Spierig",
+        "Anio": 2014
+},
+{
+        "Nombre": "Un sueño posible",
+        "Director": "John Lee Hancock",
+        "Musica": "Carter Burwell",
+        "Anio": 2009
+}]`;
+
+console.log(jsonPeliculas);
+
+const peliculas = JSON.parse(jsonPeliculas);
+
+console.log(peliculas);
+
+console.log(typeof(jsonPeliculas));
+console.log(typeof(peliculas));
